963ee1aa37a7b95df526c4cdc4b066d002d7aa0d
cristian-henrique <cristianmsbr@gmail.com>
2016-02-27 07:57:09 -0300

    Algoritmo de decodificação

---
+++ Java/Decodificacao.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+* Algoritmo de criptografia
+* @author Cristian Henrique (cristianmsbr@gmail.com)
+*/
+public class Decodificacao {
+	public static void main (String[] args) {
+		System.out.print("Digite sua mensagem > ");
+		Scanner input = new Scanner(System.in);
+		String msg = input.nextLine();
+		char[] caracteres = msg.toCharArray();
+
+		System.out.print("Digite o valor da cifra > ");
+		int cifra = input.nextInt();
+
+		for (int i = 0; i < caracteres.length; i++) {
+			int ascii = (int) caracteres[i] - cifra;
+			System.out.print((char) ascii);
+		}
+
+		System.out.println();
+	}
+}
